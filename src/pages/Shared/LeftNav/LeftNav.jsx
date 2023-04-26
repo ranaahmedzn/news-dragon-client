fe17767f23c8c5db25f37c6fc8ed4a352f82604b
Rana-Sheikh <ranasheikhsbd@gmail.com>
2023-04-26 10:56:05 +0600

    loaded and displayed categories in the left navbar

--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import './LeftNav.css'
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import first from '../../../assets/1.png'
+import second from '../../../assets/2.png'
+import third from '../../../assets/3.png'
 
 const LeftNav = () => {
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:5000/categories')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+    }, [])
+
     return (
-        <div>
-            <h2>This is LeftNav</h2>
+        <div className='mb-3'>
+            <h4 className='mb-3'>All Categories</h4>
+            <div>
+                {
+                    categories.map(category => <p
+                        key={category.id}
+                        className='category'
+                    >
+                        <Link className='text-decoration-none' to={`/category/${category.id}`}>{category.name}</Link>
+                    </p>)
+                }
+            </div>
         </div>
     );
 };
