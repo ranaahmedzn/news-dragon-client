5809de59135397e8a7a5938c6779aae510ebfcf9
Rana-Sheikh <ranasheikhsbd@gmail.com>
2023-04-30 16:33:56 +0600

    update firebase configuration file

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDUoI8hYI9z8cMKxX99UKf7f9OZxeYeKlU",
-  authDomain: "dragon-news-client-5660e.firebaseapp.com",
-  projectId: "dragon-news-client-5660e",
-  storageBucket: "dragon-news-client-5660e.appspot.com",
-  messagingSenderId: "308319374890",
-  appId: "1:308319374890:web:46521468c6482ecdecfd53"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
